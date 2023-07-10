0c00e44834d10779bfdcb92be511f161de9bc066
Roi Susi <roisusi@gmail.com>
2023-07-10 07:11:57 +0300

    del cha

--- challenge1/src/Components/Skill.js
+++
@@ -1,8 +0,0 @@
-const Skill = (props) => {
-  return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skillName}</span>
-    </div>
-  );
-};
-export default Skill;
